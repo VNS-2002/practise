16e0f73ac7ba99260abd10cbb78d762a14be37a6
VNS-2002 <vinayak12528@gmail.com>
2023-04-20 23:35:54 +0530

    First Commit

---
+++ add.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class add 
+{
+	public static void main(String[] args)
+	{
+	System.out.println("Hi..");
+	int a=4,b=5;
+	int add=a+b;
+	
+	System.out.println("var using ="+add);	
+
+	}
+} 
